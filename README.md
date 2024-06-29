592ceedcf369a663afd0b3b515eb43844856dd5e
Andrea Barghigiani <a.barghigiani@gmail.com>
2024-06-29 07:26:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Starter Project
+
+This is my custom template to quickly run React experiments already configured with Tailwind and VS Code goodies.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Start edit me and have fun</h1>
+    </div>
+  );
+}
+
+export default App;
